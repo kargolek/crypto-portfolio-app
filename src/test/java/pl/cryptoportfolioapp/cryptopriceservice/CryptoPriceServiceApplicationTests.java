6081f33917d8d1df1af1fec845510200480db061
KarolOrlowicz <karol.orlowicz@gmail.com>
2022-12-11 23:37:30 +0100

    init commit

---
+++ src/test/java/pl/cryptoportfolioapp/cryptopriceservice/CryptoPriceServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.cryptoportfolioapp.cryptopriceservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CryptoPriceServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
